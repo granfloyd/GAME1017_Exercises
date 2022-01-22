af936415c611d285b15d58068c8eb580a78990d1
granfloyd <92171589+granfloyd@users.noreply.github.com>
2022-01-22 17:13:44 -0500

    lab1video part1

--- GAME1017_W02_StartProject/GAME1017_EmptyEngine/StateManager.cpp
+++ GAME1017_W02_StartProject/GAME1017_EmptyEngine/StateManager.cpp
@@ -1,2 +1,62 @@
 #include "StateManager.h"
 
+void StateManager::Update()
+{
+	if (!s_states.empty())
+		s_states.back()->Update();//s_states.back represents the current state running
+}
+
+void StateManager::Render()
+{
+	if (!s_states.empty())
+		s_states.back()->Render();
+}
+
+void StateManager::PushState(State* pState)
+{
+	s_states.push_back(pState);//pState now becomes current state
+	s_states.back()->Enter();
+		
+}
+
+void StateManager::PopState()//used when there are 2 state in vector , i.e game and pause
+{
+	if (s_states.size() <= 1) return;
+	if (!s_states.empty())
+	{
+		s_states.back()->Exit();//cleans up curret state 
+		delete s_states.back();// deallocates current state
+		s_states.back() = nullptr;//wranle our dangle..... 
+		s_states.pop_back();//removed nullptr to deallocated current state 
+	}
+	s_states.back()->Resume();
+}
+
+void StateManager::ChangeState(State* pState)
+{
+	if (!s_states.empty())
+	{
+		s_states.back()->Exit();//cleans up curret state 
+		delete s_states.back();// deallocates current state
+		s_states.back() = nullptr;//wranle our dangle..... 
+		s_states.pop_back();//removed nullptr to deallocated current state 
+	}
+	s_states.push_back(pState);
+	s_states.back()->Enter();
+}
+
+void StateManager::Quit()
+{
+	while (!s_states.empty())
+	{
+		s_states.back()->Exit();//cleans up curret state 
+		delete s_states.back();// deallocates current state
+		s_states.back() = nullptr;//wranle our dangle..... 
+		s_states.pop_back();//removed nullptr to deallocated current state 
+	}
+}
+
+std::vector<State*>& StateManager::GetStates()
+{
+	// TODO: insert return statement here
+}
--- GAME1017_W02_StartProject/GAME1017_EmptyEngine/States.h
+++ GAME1017_W02_StartProject/GAME1017_EmptyEngine/States.h
@@ -1,7 +1,24 @@
 #pragma once
 #ifndef _STATES_H_
 #define _STATES_H_
+class State
+{
+protected: 
+	State() = default;
+public:
+	virtual void Enter() = 0;
+	virtual void Update() = 0;
+	virtual void Render();
+	virtual void Exit() = 0;
+	virtual void Resume() {};
+};
+class TilteState : public State
+{
+	
+};
+class GameState : public State
+{
 
-
-
+};
+//myarray[0].Render();
 #endif
